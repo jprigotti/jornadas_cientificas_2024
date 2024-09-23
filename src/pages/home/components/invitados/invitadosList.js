824ff5f4a48fb3756d7c82912ac6cf7230f9dc54
titojp <titojp2002@hotmail.com>
2024-09-23 13:22:06 -0300

    capcha y more updates¡

--- src/pages/home/components/invitados/invitadosList.js
+++ src/pages/home/components/invitados/invitadosList.js
@@ -1,17 +1,25 @@
 export const invitadosList = [
-  // {
-  //   id: "1",
-  //   image: "/images/invitados/sacheri.jpg",
-  //   name: "Vadim Michanchuk",
-  //   cv1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, voluptatibus!",
-  //   cv2: "",
-  // },
+  {
+    id: "1",
+    image: "/images/invitados/Vadim.png",
+    name: "Dr. Vadim Mischanchuk",
+    cv1: "Abogado panelista UBA | Especialista en temas de Derecho en Salud",
+    cv2: "Abogado UBA con título de Orientación en Derecho Penal. Especializado en temas de Derecho en Salud y Bioética. Dedicación exclusiva en Defensa Médica. Prevención de Riesgos en Medicina Legal. Docente del IDHS. Abogado en Asociación de Médicos Municipales en SEGUROS MÉDICOS S.A.",
+  },
   {
     id: "2",
-    image: "/images/invitados/sacheri.jpg",
+    image: "/images/invitados/Sacheri.png",
     name: "Prof. Eduardo Sacheri",
-    cv1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, voluptatibus!",
-    cv2: "",
+    cv1: "Profesor y Licenciado en Historia | Guionista y colabora en diarios y revistas nacionales e internacionales",
+    cv2: `Autor de cuentos y novelas, toda su obra se encuentra en Alfaguara: 
+          los libros de relatos Esperándolo a Tito, Te conozco, 
+          Mendizábal, Lo raro empezó después, Un viejo que se pone de pie,
+          La noche de la Usina (Premio Alfaguara de Novela 2016), Lo mucho
+          que te amé, El funcionamiento general del mundo y Nosotros dos en
+          la tormenta.
+          Aráoz y la verdad fue adaptada al teatro y La pregunta de sus
+          ojos, Papeles en el viento y La noche de la Usina fueron llevadas
+          al cine. Su obra ha sido traducida a más de veinte idiomas. 
+          En 2022 publicó Los días de la Revolución (1806-1820), el primero de sus libros de divulgación histórica."`,
   },
 ];
-

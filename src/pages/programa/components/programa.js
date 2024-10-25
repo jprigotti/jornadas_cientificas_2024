2a92b6e85a57836c42558b935707ce79f3822797
jnardi86 <julian.nardi@gmail.com>
2024-10-25 11:10:22 -0300

    working on programa

---
+++ src/pages/programa/components/programa.js
@@ -0,0 +1,78 @@
+export const programa = [
+
+]
+
+
+
+
+/**
+inscripcion_card: {
+categoria: "inscripcion",
+dia:
+hora: 
+ubicacion:
+descripcion:
+} 
+
+apertura: {
+categoria: "apertura",
+dia:
+hora: 
+ubicacion:
+descripcion:
+}
+
+conferencia: {
+categoria: "conferencia"
+dia:
+hora: 
+ubicacion:
+titulo:
+orador:
+}
+
+vino_honor: {
+categoria: "vino_de_honor"
+dia:
+hora: 
+ubicacion:
+}
+
+temas_libres: {
+category: "temas_libres"
+dia:
+hora: 
+ubicacion: 
+discutidores:
+secretaria:
+trabajos: [
+            {titulo,
+            abstract}]
+
+}
+
+trabajos_premio: {
+}
+
+break: {
+categoria: "break"
+hora: }
+
+mesa_redonda: {
+categoria: "mesa_redonda"
+dia:
+hora:
+ubicacion:
+titulo:
+presidente: "",
+secretarios: [{
+                nombre: "",
+                servicio: ""}],
+disertantes: [{
+                nombre: "",
+                servicio: "",
+                tema: ""}]
+
+}
+
+ */

00e13eecba2eca55d61476b092a6cc1ac3be6a0c
titojp <titojp2002@hotmail.com>
2025-01-24 08:22:17 -0300

    publishing

--- src/pages/perfil/components/Certificates.jsx
+++ src/pages/perfil/components/Certificates.jsx
@@ -2,15 +2,13 @@ import { useGenerateCertificados } from "../hooks/useGenerateCertificados";
 import { useRegistration } from "../hooks/useRegistration";
 import { useAuth } from "../../../core/auth/hooks/useAuth";
 import { useProfile } from "../hooks/useProfile";
-import ButtonVioletSM from "../../../components/buttons/ButtonVioletSM"
+import ButtonVioletSM from "../../../components/buttons/ButtonVioletSM";
 
 const Certificates = () => {
   const { user } = useAuth();
   const { userData } = useProfile(user.uid);
-  const { userRegistration, isSubmitting } =
-    useRegistration();
-  const { generatePDF } = useGenerateCertificados()
-
+  const { userRegistration, isSubmitting } = useRegistration();
+  const { generatePDF } = useGenerateCertificados();
 
   return (
     <div className="laptop1:ms-40 bg-White flex flex-col items-center py-10 px-3">
@@ -19,14 +17,20 @@ const Certificates = () => {
           {/* <WarningIcon width={"40px"} height={"40px"} /> */}
           <h1 className="text-xl ps-5">Mis certificados</h1>
         </div>
-        <div className="flex">
+        <div className="flex flex-col items center p-5">
           <p>Certificado de participación: </p>
-
-          <ButtonVioletSM onClick={() => generatePDF(userData)} label={"Descargar"} disabled={useRegistration?.payment == "paid" ? "disabled" : "enabled"} />
-
+          <div className="py-5 self-center">
+            <ButtonVioletSM
+              onClick={() => generatePDF(userData)}
+              label={"Descargar"}
+              disabled={
+                useRegistration?.payment == "paid" ? "disabled" : "enabled"
+              }
+            />
+          </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
